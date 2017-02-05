140908fa754175219303649846435b4ffd7b1db4
Mouse.S <littlemouse.song@gmail.com>
2017-02-05 15:46:39 +0800

    Added done status

--- playNine.jsx
+++ playNine.jsx
@@ -1,3 +1,21 @@
+var possibleCombinationSum = function (arr, n) {
+    if (arr.indexOf(n) >= 0) { return true; }
+    if (arr[0] > n) { return false; }
+    if (arr[arr.length - 1] > n) {
+        arr.pop();
+        return possibleCombinationSum(arr, n);
+    }
+    var listSize = arr.length, combinationsCount = (1 << listSize)
+    for (var i = 1; i < combinationsCount; i++) {
+        var combinationSum = 0;
+        for (var j = 0; j < listSize; j++) {
+            if (i & (1 << j)) { combinationSum += arr[j]; }
+        }
+        if (n === combinationSum) { return true; }
+    }
+    return false;
+};
+
 var StarsFrame = React.createClass({
     render: function () {
         var stars = [];
@@ -107,6 +125,8 @@ var DoneFrame = React.createClass({
         return (
             <div className="well text-center">
                 <h2>{this.props.doneStatus}</h2>
+                <button className="btn btn-default"
+                    onClick={this.props.resetGame}>Play again!</button>
             </div>
         );
     }
@@ -122,6 +142,9 @@ var Game = React.createClass({
             correct: null,
             doneStatus: null
         };
+    },
+    resetGame: function () {
+        this.replaceState(this.getInitialState());
     },
     randomNumber: function () {
         return Math.floor(Math.random() * 9) + 1
@@ -163,6 +186,8 @@ var Game = React.createClass({
             usedNumbers: usedNumbers,
             correct: null,
             numberOfStars: this.randomNumber()
+        }, function () {
+            this.updateDoneStatus();
         });
 
     },
@@ -173,15 +198,30 @@ var Game = React.createClass({
                 correct: null,
                 selectedNumbers: [],
                 redraws: this.state.redraws - 1
-            })
+            }, function () {
+                this.updateDoneStatus();
+            });
         }
+    },
+    possibleSolutions: function () {
+        var numberOfStars = this.state.numberOfStars,
+            possibleNumbers = [],
+            usedNumbers = this.state.usedNumbers;
+
+        for (var i = 0; i <= 9; i++) {
+            if (usedNumbers.indexOf(i) < 0) {
+                possibleNumbers.push(i);
+            }
+        }
+
+        return possibleCombinationSum(possibleNumbers, numberOfStars);
     },
     updateDoneStatus: function () {
         if (this.state.usedNumbers.length === 9) {
             this.setState({ doneStatus: 'Done. Nice!' });
             return;
         }
-        if (!this.possibleSolutions()) {
+        if (this.state.redraws === 0 && !this.possibleSolutions()) {
             this.setState({ doneStatus: 'Game Over!' });
         }
 
@@ -196,7 +236,8 @@ var Game = React.createClass({
             bottomFrame;
 
         if (doneStatus) {
-            bottomFrame = <DoneFrame doneStatus={doneStatus} />;
+            bottomFrame = <DoneFrame doneStatus={doneStatus}
+                resetGame={this.resetGame} />;
         } else {
             bottomFrame = <NumbersFrame selectedNumbers={selectedNumbers}
                 selectNumber={this.selectNumber}
